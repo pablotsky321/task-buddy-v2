006d65d574d9ecaa7b6037d59c2787bdf5edb174
pablotsky321 <jprestrepo346@gmail.com>
2026-01-12 19:41:56 -0500

    fix: changed url deploying

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    API_URL: 'http://ec2-3-143-209-161.us-east-2.compute.amazonaws.com:80'
+    API_URL: 'https://ec2-3-143-209-161.us-east-2.compute.amazonaws.com:443'
 };
